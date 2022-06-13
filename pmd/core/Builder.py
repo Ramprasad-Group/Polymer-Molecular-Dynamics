830de8d6c5668bd916f5899a08b8874e76fd738d
madhubantimukherjee <31185879+madhubantimukherjee@users.noreply.github.com>
2022-06-13 14:38:04 -0400

    Fix PSP builder `outdir` arg bug

--- pmd/core/Builder.py
+++ pmd/core/Builder.py
@@ -231,7 +231,7 @@ class PSP(Builder):
             with HiddenPrints():
                 amor = ab.Builder(pd.DataFrame(data=input_data),
                                   density=density,
-                                  OutDir=output_dir)
+                                  outdir=output_dir)
                 amor.Build()
 
                 if self._is_opls_force_field:
@@ -293,7 +293,7 @@ class PSP(Builder):
             'RightCap': ['[*][H]']
         }
         self._run_psp(input_data, density, data_fname, output_dir, cleanup)
-
+_
     def write_solvent_data(self, output_dir, smiles, solvent_smiles, density,
                            natoms_total, length, nsolvents, nchains,
                            data_fname, cleanup):
